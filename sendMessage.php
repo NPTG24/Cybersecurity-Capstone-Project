f9320428ad2d80aaf165b6812cc8763853de39ad
NPTG <felipecanalescayuqueo@gmail.com>
2021-04-17 19:56:39 -0400

    encrypted messages

--- sendMessage.php
+++ sendMessage.php
@@ -8,7 +8,20 @@ if($_POST)
 	$to=$_POST['to'];
     $msg=$_POST['msg'];
     
-	$sql="INSERT INTO `chat`(`name`,`receives`,`message`) VALUES ('".$name."','".$to."', '".$msg."')";
+
+	function encrypt($msg, $encrypt_key){
+		$key = hex2bin($encrypt_key);
+		$nonceSize = openssl_cipher_iv_length('aes-256-ctr');
+		$nonce = openssl_random_pseudo_bytes($nonceSize);
+
+		$ciphertext = openssl_encrypt($msg,'aes-256-ctr',$key,OPENSSL_RAW_DATA,$nonce);
+		return base64_encode($nonce.$ciphertext);
+	}
+	$private_secret_key = '1f4276378ad3214c873928dbef42743f';
+	$encrypted = encrypt($msg, $private_secret_key);
+
+	$sql="INSERT INTO `chat`(`name`,`receives`,`message`) VALUES ('".$name."','".$to."', '".$encrypted."')";
+
 
 	$query = mysqli_query($conn,$sql);
 	if($query)
@@ -20,5 +33,5 @@ if($_POST)
 		echo "Something went wrong";
 	}
 	
-	}
+}
 ?>
