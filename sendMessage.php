d2b7104528ec5a790bb5e48057e6b8963187617c
NPTG <felipecanalescayuqueo@gmail.com>
2021-04-18 03:49:53 -0400

    send message only to existing users

--- sendMessage.php
+++ sendMessage.php
@@ -4,34 +4,50 @@ include "config.php";
 session_start();
 if($_POST)
 {
-	$name=$_SESSION['name'];
+	$user=$_SESSION['name'];
 	$to=$_POST['to'];
     $msg=$_POST['msg'];
-    
 
-	function encrypt($msg, $encrypt_key){
-		$key = hex2bin($encrypt_key);
-		$nonceSize = openssl_cipher_iv_length('aes-256-ctr');
-		$nonce = openssl_random_pseudo_bytes($nonceSize);
-
-		$ciphertext = openssl_encrypt($msg,'aes-256-ctr',$key,OPENSSL_RAW_DATA,$nonce);
-		return base64_encode($nonce.$ciphertext);
+	$verify = "SELECT * FROM register WHERE name = '$to'";
+	$verify_user = mysqli_query($conn, $verify);
+	if(mysqli_num_rows($verify_user) > 0){
+		function encrypt($msg, $encrypt_key){
+			$key = hex2bin($encrypt_key);
+			$nonceSize = openssl_cipher_iv_length('aes-256-ctr');
+			$nonce = openssl_random_pseudo_bytes($nonceSize);
+	
+			$ciphertext = openssl_encrypt($msg,'aes-256-ctr',$key,OPENSSL_RAW_DATA,$nonce);
+			return base64_encode($nonce.$ciphertext);
+		}
+		$private_secret_key = '1f4276378ad3214c873928dbef42743f';
+		$encrypted = encrypt($msg, $private_secret_key);
+	
+		$sql="INSERT INTO `chat`(`name`,`receives`,`message`) VALUES ('".$user."','".$to."', '".$encrypted."')";
+		echo '
+		<script> 
+			alert("Message sent succesfully"); 
+			window.location = "/chat_project/sendMessage.php";
+		</script>';
+	
+	
+		$query = mysqli_query($conn,$sql);
+		if($query)
+		{
+			header('Location: chatpage.php');
+		}
+		else
+		{
+			echo "Something went wrong";
+		}
 	}
-	$private_secret_key = '1f4276378ad3214c873928dbef42743f';
-	$encrypted = encrypt($msg, $private_secret_key);
-
-	$sql="INSERT INTO `chat`(`name`,`receives`,`message`) VALUES ('".$name."','".$to."', '".$encrypted."')";
-
-
-	$query = mysqli_query($conn,$sql);
-	if($query)
-	{
-		header('Location: chatpage.php');
+	else{
+		echo '
+		<script> 
+			alert("Username does not exist!"); 
+			window.location = "/chat_project/home.php";
+		</script>';
 	}
-	else
-	{
-		echo "Something went wrong";
-	}
+	
 	
 }
 else
