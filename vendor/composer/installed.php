17466926bbca1b4ce4191f8b6e4fc5c97d51986e
NPTG <felipecanalescayuqueo@gmail.com>
2021-04-19 08:33:05 -0400

    json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1e35f7e3be12a030cc8f2155457be0f8ed0a0ff8',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1e35f7e3be12a030cc8f2155457be0f8ed0a0ff8',
+    ),
+  ),
+);
