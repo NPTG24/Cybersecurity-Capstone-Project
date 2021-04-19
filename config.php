80e7c969519636c6f2477fda945394756efb683f
NPTG <felipecanalescayuqueo@gmail.com>
2021-04-19 01:52:14 -0400

    add substr

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $dbHost =$url['us-cdbr-east-03.cleardb.com'];
 $dbUsername =$url['bfb973d3caa39c'];
 $dbPassword =$url['41b2fe72'];
-$dbDatabase =$url['heroku_a3a45f89798acbc'];
+$dbDatabase =substr($url['heroku_a3a45f89798acbc'],1);
 $conn=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbDatabase);
 
 ?>
