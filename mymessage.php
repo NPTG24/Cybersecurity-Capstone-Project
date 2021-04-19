c98894442f7142c5eebb5533f469d01a07fc02d6
NPTG <felipecanalescayuqueo@gmail.com>
2021-04-19 07:18:57 -0400

    test3

--- mymessage.php
+++ mymessage.php
@@ -24,10 +24,20 @@
   </tr>
   
   <?php
-  
+
+  function decrypt($msg, $encrypt_key){
+    $key = hex2bin($encrypt_key);
+    $msg = base64_decode($msg);
+    $nonceSize = openssl_cipher_iv_length('aes-256-ctr');
+    $nonce = mb_substr($msg,0,$nonceSize,'8bit');
+    $ciphertext = mb_substr($msg,$nonceSize,null,'8bit');
+
+    $plaintext = openssl_decrypt($ciphertext,'aes-256-ctr',$key,OPENSSL_RAW_DATA,$nonce);
+    return $plaintext;
+  }
+
   $i = 0; 
   while($row = mysqli_fetch_array($res)){
-    print('hola');
     $msg=$row['message'];
     $decrypted=decrypt($msg, $private_secret_key);
     ?>
@@ -82,3 +92,4 @@ color:white;
 	}
 
 </style>
+
