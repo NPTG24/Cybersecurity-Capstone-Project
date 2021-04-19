242a43538b888c9da04b20f3a8ff08e1e695996b
NPTG <felipecanalescayuqueo@gmail.com>
2021-04-19 00:45:09 -0400

    connect db heroku

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$dbHost =$url['localhost'];
-$dbUsername =$url['root'];
-$dbPassword =$url[''];
-$dbDatabase =$url['proyect_db'];
+$dbHost =$url['us-cdbr-east-03.cleardb.com'];
+$dbUsername =$url['bfb973d3caa39c'];
+$dbPassword =$url['41b2fe72'];
+$dbDatabase =$url['heroku_a3a45f89798acbc'];
 $conn=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbDatabase);
 
 ?>
