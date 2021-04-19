b20a5b3d9498ebd199b33a2a29ffbea6bd557d57
NPTG <felipecanalescayuqueo@gmail.com>
2021-04-19 01:54:25 -0400

    add active group

--- config.php
+++ config.php
@@ -5,6 +5,10 @@ $dbHost =$url['us-cdbr-east-03.cleardb.com'];
 $dbUsername =$url['bfb973d3caa39c'];
 $dbPassword =$url['41b2fe72'];
 $dbDatabase =substr($url['heroku_a3a45f89798acbc'],1);
+
+$active_group = 'default';
+$query_builder = TRUE;
+
 $conn=mysqli_connect($dbHost,$dbUsername,$dbPassword,$dbDatabase);
 
 ?>
